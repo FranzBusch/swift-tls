6c32ecfdfebb943764eb2bd62a2ec17a87a42d1b
Franz Busch <f.busch@apple.com>
2026-06-17 09:38:55 -0700

    Fix crypto dep

--- Package.swift
+++ Package.swift
@@ -61,7 +61,7 @@ var settings: [SwiftSetting]? = [
 #if os(Linux)
 packageDependencies = [
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", "3.6.1" ..< "4.0.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", from: "5.0.0-beta.1"),
     .package(url: "https://github.com/FranzBusch/swift-binary-parsing.git", branch: "fb-networking"),
 ]
 targetDependencies = [
